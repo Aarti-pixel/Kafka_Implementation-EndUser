3fde7d24d906702c19f093e9ef1ef16c0af38da2
Aarti-pixel <abj.aarti@gmail.com>
2025-12-30 19:46:30 +0530

    "removed logger"

--- src/main/java/com/example/kafka/endUser/EndUserApplication.java
+++ src/main/java/com/example/kafka/endUser/EndUserApplication.java
@@ -1,9 +1,6 @@
 package com.example.kafka.endUser;
 
 import com.example.kafka.endUser.config.AppConstants;
-import org.apache.kafka.common.internals.Topic;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -14,8 +11,6 @@ public class EndUserApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EndUserApplication.class, args);
 	}
-
-    private Logger logger= LoggerFactory.getLogger(EndUserApplication.class);
 
     @KafkaListener(topics = AppConstants.TOPICNAME,groupId = AppConstants.GroupId)
     private String updatedMessage(String message){
